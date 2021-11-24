e40fd006de64eaeb0c3e4e8e6d97c40c1edc276a
Gulshan Yadav <gulshanyadav@Gulshans-MacBook-Pro.local>
2021-11-24 23:11:42 +0530

    initial commit

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,6 +1,6 @@
 from django.db.models import fields
-from rest_framework import serializers 
-from django.contrib.auth.models import User 
+from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Product, Order, OrderItem, ShippingAddress
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -21,6 +21,7 @@ class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = '__all__'
+
 
 class OrderSerializer(serializers.ModelSerializer):
     orderItems = serializers.SerializerMethodField(read_only=True)
@@ -50,7 +51,6 @@ class OrderSerializer(serializers.ModelSerializer):
         return serializer.data
 
 
-
 class UserSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField(read_only=True)
     _id = serializers.SerializerMethodField(read_only=True)
@@ -59,13 +59,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin']
-    
+
     def get__id(self, obj):
         return obj.id
-    
+
     def get_isAdmin(self, obj):
         return obj.is_staff
-    
+
     def get_name(self, obj):
         name = obj.first_name
         if name == '':
@@ -75,9 +75,10 @@ class UserSerializer(serializers.ModelSerializer):
 
 class UserSerializerWithToken(UserSerializer):
     token = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = User
-        fields =['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
 
     def get_token(self, obj):
         token = RefreshToken.for_user(obj)
